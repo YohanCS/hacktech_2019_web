a39a89d7930495700289f4e0abb57ed38a590e0c
Lasse Nordahl <lasseanordahl@gmail.com>
2019-03-02 10:59:25 -0800

    font change

--- src/components/dashboard/internCards/InternshipCard.js
+++ src/components/dashboard/internCards/InternshipCard.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+
 import Paper from '@material-ui/core/Paper';
 
 import styles from './../../../css/app.module.css';
+
+import placeholder_image from './../../../assets/test.jpg'
 
 class InternshipCard extends Component {
     constructor(props) {
@@ -26,7 +30,12 @@ class InternshipCard extends Component {
 		return (
             <Paper className={styles.applicationCard}>
                 <Card>
-                    <img src={this.getImageSource(applicationData.domain)}/>
+                    <CardMedia
+                        className={styles.companyIcon}
+                        title="Company Icon"
+                    >
+                        <img src={this.getImageSource(applicationData.domain)}/>
+                    </CardMedia>
                     <CardContent>
                         {applicationData.name}
                     </CardContent>
